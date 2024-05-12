b1b11c998339a39e46074e1c9b2b296e19735144
Doğukan Özyurt <dogukanzyrt@outlook.com>
2024-05-12 15:27:33 +0300

    admin add skill

---
+++ Core_UdemyProje/Controllers/SkillController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_UdemyProje.Controllers
+{
+    public class SkillController : Controller
+    {
+
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+
+
+
+        public IActionResult Index()
+        {
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+    }
+}
