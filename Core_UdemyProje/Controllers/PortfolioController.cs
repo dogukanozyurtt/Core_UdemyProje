73cb86778f3b19ac9fb0ecd429e26342390497fa
Doğukan Özyurt <dogukanzyrt@outlook.com>
2024-06-01 14:18:39 +0300

    Portfolio crud and validation

--- Core_UdemyProje/Controllers/PortfolioController.cs
+++ Core_UdemyProje/Controllers/PortfolioController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_UdemyProje.Controllers
@@ -9,6 +11,7 @@ namespace Core_UdemyProje.Controllers
     {
         PortfolioMananager portfolioMananager = new PortfolioMananager(new EfPortfolioDal());
 
+        //Portfoli Index
         public IActionResult Index()
         {
             ViewBag.v1 = "Proje Listesi";
@@ -18,21 +21,86 @@ namespace Core_UdemyProje.Controllers
             return View(values);
         }
 
+        //Portfoli add Get
         [HttpGet]
         public IActionResult AddPortfolio()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult AddPortfolio(Portfolio p)
         {
             ViewBag.v1 = "Proje Listesi";
             ViewBag.v2 = "Projelerim";
             ViewBag.v3 = "Proje Ekleme";
-            portfolioMananager.TAdd(p);
+            return View();
+        }
+
+
+        //Portfoli add Post
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio p)
+        {
+
+
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult result = validations.Validate(p);
+            if (result.IsValid)
+            {
+                portfolioMananager.TAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+
+
+        // Portfolio Delete 
+        public IActionResult DeletePortfolio(int id)
+        {
+            var values = portfolioMananager.TGetByID(id);
+            portfolioMananager.TDelete(values);
             return RedirectToAction("Index");
         }
+
+
+
+        // Portfolio Edit 
+        [HttpGet]
+        public IActionResult EditPortfolio(int id)
+        {
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Düzenleme";
+            var values = portfolioMananager.TGetByID(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditPortfolio(Portfolio portfolio)
+        {
+
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult result = validations.Validate(portfolio);
+            if (result.IsValid)
+            {
+                portfolioMananager.TUpdate(portfolio);
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
+        }
+
 
     }
 }
