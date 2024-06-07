e66c2154e46e833d023fd126e8327fd7424c26fa
Doğukan Özyurt <dogukanzyrt@outlook.com>
2024-06-07 15:52:02 +0300

    new thema update

---
+++ Core_UdemyProje/Controllers/ServiceController.cs
@@ -0,0 +1,69 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_UdemyProje.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+      
+        // Index
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Hizmet Listesi";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmet Listesi";
+            var values = serviceManager.TGetList();
+
+            return View(values);
+        }
+
+        // Service Add
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            ViewBag.v1 = "Hizmet Ekleme";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmet Ekleme";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            serviceManager.TAdd(service);
+            return RedirectToAction("Index");
+        }
+
+
+        // Delete Service
+        public IActionResult DeleteService(int id)
+        {
+            var values = serviceManager.TGetByID(id);
+            serviceManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+
+        // Service Edit
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            ViewBag.v1 = "Hizmet Ekleme";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmet Ekleme";
+            var values = serviceManager.TGetByID(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditService(Service service)
+        {
+            serviceManager.TUpdate(service);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
