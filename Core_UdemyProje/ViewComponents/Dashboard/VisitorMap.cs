68f2ead6ccf8c5f1dea3c049c7a139c90f470d16
Doğukan Özyurt <dogukanzyrt@outlook.com>
2024-06-14 15:17:44 +0300

    new user dashboard

---
+++ Core_UdemyProje/ViewComponents/Dashboard/VisitorMap.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_UdemyProje.ViewComponents.Dashboard
+{
+    public class VisitorMap:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+
+    }
+}
